2b4db88d3b21568f9d98522db894a4c48dff62b3
Jie Li <lijie_12@hotmail.com>
2023-09-14 13:51:05 +0100

    build the structure of the project

---
+++ src/autocpd/utils.py
@@ -0,0 +1,20 @@
+"""
+Author         : Jie Li, Department of Statistics, London School of Economics.
+Date           : 2023-09-04 14:04:33
+Last Revision  : 2023-09-14 10:30:47
+Last Author    : Jie Li
+File Path      : /AutoCPD/src/utils.py
+Description    :
+
+
+
+
+
+
+
+
+Copyright (c) 2023 by Jie Li, j.li196@lse.ac.uk
+All Rights Reserved.
+"""
+def add_one(number):
+    return number + 1
