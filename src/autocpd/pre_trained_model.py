07c95da85528b6dd2205af0b9ad25c72c8d5b48d
Jie Li <lijie_12@hotmail.com>
2023-09-25 18:21:58 +0100

    update the version and solve the  relative path

--- src/autocpd/pre_trained_model.py
+++ src/autocpd/pre_trained_model.py
@@ -1,7 +1,7 @@
 """
 Author         : Jie Li, Department of Statistics, London School of Economics.
 Date           : 2023-09-25 10:56:59
-Last Revision  : 2023-09-25 11:14:49
+Last Revision  : 2023-09-25 11:23:28
 Last Author    : Jie Li
 File Path      : /AutoCPD/src/autocpd/pre_trained_model.py
 Description    :
@@ -20,14 +20,18 @@ import
 import tensorflow as tf
 
 
-def load_pretrained_model():
+def load_pretrained_model(path):
     """
     Load the pretrained model
 
+    Parameters
+    ----------
+    path : str
+        the path of pre-trained model
     Returns
     -------
     tf.Model
         the pre-trained model.
     """
-    model = tf.keras.models.load_model("./Demo/model")
+    model = tf.keras.models.load_model(path)
     return model
--- test/test_load_pretrained_model.py
+++ test/test_load_pretrained_model.py
@@ -1,7 +1,7 @@
 """
 Author         : Jie Li, Department of Statistics, London School of Economics.
 Date           : 2023-09-25 11:11:18
-Last Revision  : 2023-09-25 11:12:55
+Last Revision  : 2023-09-25 18:21:53
 Last Author    : Jie Li
 File Path      : /AutoCPD/test/test_load_pretrained_model.py
 Description    :
@@ -16,6 +16,13 @@ Description    :
 Copyright (c) 2023 by Jie Li, j.li196@lse.ac.uk
 All Rights Reserved.
 """
+import os
+import pathlib
+
+import autocpd
 from autocpd.pre_trained_model import load_pretrained_model
 
-model = load_pretrained_model()
+root_path = os.path.dirname(autocpd.__file__)
+model_path = pathlib.Path(root_path, "Demo", "model")
+
+model = load_pretrained_model(model_path)
