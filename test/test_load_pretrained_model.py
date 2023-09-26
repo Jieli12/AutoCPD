e4f457d55a23b233d46c640839ef715e61e3eeb3
Jie Li <lijie_12@hotmail.com>
2023-09-26 15:30:29 +0100

    save

--- test/test_load_pretrained_model.py
+++ test/test_load_pretrained_model.py
@@ -1,7 +1,7 @@
 """
 Author         : Jie Li, Department of Statistics, London School of Economics.
 Date           : 2023-09-25 11:11:18
-Last Revision  : 2023-09-25 18:21:53
+Last Revision  : 2023-09-26 15:21:02
 Last Author    : Jie Li
 File Path      : /AutoCPD/test/test_load_pretrained_model.py
 Description    :
@@ -19,6 +19,9 @@ All Rights Reserved.
 import os
 import pathlib
 
+import numpy as np
+import tensorflow as tf
+
 import autocpd
 from autocpd.pre_trained_model import load_pretrained_model
 
@@ -26,3 +29,21 @@ root_path = os.path.dirname(autocpd.__file__)
 model_path = pathlib.Path(root_path, "Demo", "model")
 
 model = load_pretrained_model(model_path)
+model.summary()
+
+datapath = pathlib.Path(root_path, "HASC")
+np.random.seed(2022)  # numpy seed fixing
+tf.random.set_seed(2022)  # tensorflow seed fixing
+subjects = [
+    "person101",
+    "person102",
+    "person103",
+    "person104",
+    "person105",
+    "person106",
+    "person107",
+]
+# %% load the trained model
+logdir = pathlib.Path("tensorboard_logs", "Trial")
+label_dict = np.load(Path(logdir, model_name, "label_dict.npy"), allow_pickle=True)
+label_dict = label_dict.item()
